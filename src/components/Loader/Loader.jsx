045c88cf8777984e7e93173ceb76028981ffb2f8
MuhammadAkbar <fotismenos007@mail.ru>
2022-07-28 18:34:44 +0500

    Loader added

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import './loader.css'
+
+/*
+
+https://tobiasahlin.com/spinkit/
+
+*/
+
+const Loader = () => {
+  return (
+    <div className='spinner'></div>
+  )
+}
+
+export default Loader
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,13 +2,14 @@ import { useSelector } from 'react-redux'
 
 import Post from '../Posts/Post/Post'
 
+import Loader from '../Loader/Loader'
+
 const Posts = ({ setCurrentId }) => {
 
   const posts = useSelector(state => state.posts)
-
   return (
     <div>
-      {!posts.length ? 'Loading' : <>
+      {!posts.length ? <Loader /> : <>
         <div className="row row-cols-1 row-cols-md-3 g-4">
           {posts.map(post => (
             <Post post={post} setCurrentId={setCurrentId} key={post._id} />
